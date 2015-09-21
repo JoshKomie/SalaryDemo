ad1722584bf6a95b2b3dd83acf25a1618bc57da7
brianortiz89@gmail.com <brian.ortiz540@sie-1222-05.csuci.edu>
2015-09-21 13:09:21 -0700

    initial

---
+++ src/salarydemo/StudentEmployee.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salarydemo;
+
+/**
+ *
+ * @author brian.ortiz540
+ */
+public class StudentEmployee {
+       /**
+      Constructs an hourly employee with a given name and weekly wage.
+      @param name the name of this employee
+      @param wage the hourly wage 
+   */
+   public StudentEmployee(String name, double wage)
+   {
+      setName(name);
+      hourlyWage = wage;
+   }
+ 
+   public double weeklyPay(int hoursWorked)
+   {
+      double pay = hoursWorked * hourlyWage;
+      if (hoursWorked > 40)
+      {
+         // Add overtime
+         pay = pay + ((hoursWorked - 40) * 0.5) * hourlyWage;
+      }
+      return pay;
+   }
+}
